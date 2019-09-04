84eb0f85b39418e17df2260ce0a583ad8dfe9b4f
Diego Costa Pereira <diegoasp@gmail.com>
2019-09-04 20:27:24 -0300

    ligação dos componentes do storyboard ao viewcontroller

--- Conversores/Conversores/ViewController.swift
+++ Conversores/Conversores/ViewController.swift
@@ -2,8 +2,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    @IBOutlet weak var lbUnit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
 }
